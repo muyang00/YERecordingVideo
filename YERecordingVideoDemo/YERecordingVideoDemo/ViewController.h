e8968e5db356cf6e1775152aa0d1741e5eee4f39
muyang00 <280798744@qq.com>
2017-03-17 15:28:32 +0800

    先提交一部分

---
+++ YERecordingVideoDemo/YERecordingVideoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YERecordingVideoDemo
+//
+//  Created by yongen on 17/3/17.
+//  Copyright © 2017年 yongen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
